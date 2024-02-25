9f60d657f469e2c888e2bf966c692302716ed1ad
mariahjv <mariah.vadak@ou.edu>
2024-02-24 19:00:04 -0600

    Implement q-learning algorithm

--- q_learning.py
+++ q_learning.py
@@ -1,15 +1,59 @@
 import gym
+import numpy as np
+import matplotlib.pyplot as plt
 
 # S = start, F = frozen, H = Hole, G = Goal
 
-# Initialize FrozenLake environment with the default 4x4 map
-environment = gym.make('FrozenLake-v1', render_mode="human", is_slippery=False)
+def main(num_episodes):
+    # Initialize FrozenLake environment with the default 4x4 map
+    environment = gym.make('FrozenLake-v1', map_name="4x4", render_mode="False", is_slippery="False")
+    # initialize q-learning table(4x4)
+    Q = np.zeros((environment.observation_space.n, environment.action_space.n)) 
+    # discount factor
+    gamma = 0.9 
+    # learning rate   
+    alpha = 0.1 
+    # exploration rate  
+    epsilon = 0.5  
+    random_num = np.random.default_rng()
+    episode_rewards = np.zeros(num_episodes)
 
-# Resets the environment to an initial state
-init_state = environment.reset()
+    print("Start environmant")
+    for i in range(num_episodes):
+        # Resets the environment to an initial state; returns tuple
+        state = environment.reset()[0]
 
-# Renders the environment to help visualize what the agent sees
-environment.render()
+        terminated = False
+        truncated = False
+
+        while not terminated and not truncated:
+            # Renders the environment to help visualize what the agent sees
+            environment.render()
+
+            if random_num.random() < epsilon:
+                action = environment.action_space.sample()
+            else:
+                action = np.argmax(Q[state, :])
+
+            next_state, reward, terminated, truncated, info = environment.step(action)
+
+            # Q-learning formula to update
+            Q[state, action] = Q[state, action] + alpha * (reward + gamma * (np.max(Q[next_state, :]) - Q[state, action]))
+            state = next_state
+
+        if reward == 1:
+            episode_rewards[i] = 1
+
+        # print(f"Action: {action}, Observation: {observation}, Reward: {reward}, Truncated: {truncated}, Info: {info}")
+
+        if terminated or truncated:
+            state = environment.reset()[0]
+        
+    environment.close()
+
+
+if __name__ == '__main__':
+    main(1500)
 
 # step through manually
 # while True:
@@ -21,16 +65,3 @@ environment.render()
 #     environment.render()
 #     if done:
 #         break
-
-observation, info = environment.reset()
-
-for _ in range(25):
-    action = environment.action_space.sample()  # agent policy that uses the observation and info
-    observation, reward, terminated, truncated, info = environment.step(action)
-
-    print(f"Action: {action}, Observation: {observation}, Reward: {reward}, Truncated: {truncated}, Info: {info}")
-
-    if terminated or truncated:
-        observation, info = environment.reset()
-    
-environment.close()
